5403631ca32db89ed0ac85b0781a023de7054c44
Willem Meijer <w.meijer.5@student.rug.nl>
2022-06-04 09:01:31 +0000

    added standard maven project

---
+++ src/main/java/com/wmeijer221/autoGithubUpdater/App.java
@@ -0,0 +1,13 @@
+package com.wmeijer221.autoGithubUpdater;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
